71d6f06eaf9f82696fa885ac7a14cf1a477d3a20
SOUnit <workbookwork7@gmail.com>
2021-02-23 19:42:10 +0900

    added props to component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail author='sam' />
-      <CommentDetail author='tom' />
-      <CommentDetail author='jane' />
+      <CommentDetail
+        author='sam'
+        timeAgo='Today at 4:00PM'
+        avatar={faker.image.image()}
+        content='test1'
+      />
+      <CommentDetail
+        author='tom'
+        timeAgo='Today at 2:30AM'
+        avatar={faker.image.image()}
+        content='test2'
+      />
+      <CommentDetail
+        author='jane'
+        timeAgo='Yesterday at 6:00PM'
+        avatar={faker.image.image()}
+        content='test3'
+      />
     </div>
   );
 };
